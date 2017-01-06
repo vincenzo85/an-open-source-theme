68afd1695f7f8be4e6dcb7c25b0d073c81cc28ad
vincenzo85 <vincenzo.difranco@gmail.com>
2017-01-06 21:21:57 +0100

    added css e font awesome

--- header.php
+++ header.php
@@ -12,11 +12,33 @@
 ?><!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 <head>
+	
 <meta charset="<?php bloginfo( 'charset' ); ?>">
 <meta name="viewport" content="width=device-width, initial-scale=1">
+<title><?php wp_title( '|', true, 'right' ); ?></title>
 <link rel="profile" href="http://gmpg.org/xfn/11">
+<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+
+<!-- Bootstrap core CSS -->
+<link href="<?php bloginfo('stylesheet_directory'); ?>/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+<!-- Google Fonts -->
+    <link href="<?php bloginfo('stylesheet_directory'); ?>/vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
+    <link href='https://fonts.googleapis.com/css?family=Kaushan+Script' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Droid+Serif:400,700,400italic,700italic' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Roboto+Slab:400,100,300,700' rel='stylesheet' type='text/css'>
+	
+ <!-- Theme CSS -->
+	<link href="<?php bloginfo('stylesheet_directory'); ?>/css/agency.min.css" rel="stylesheet">
 
 <?php wp_head(); ?>
+
+<!-- HTML5 shiv and Respond.js IE8 support of HTML5 elements and media queries -->
+<!--[if lt IE 9]>
+	<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+	<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+<![endif]-->
 </head>
 
 <body <?php body_class(); ?>>
